f7cf3fd939bbe08cca7a57a4cb19ffa110f957b7
David <davidtucu@hotmail.com>
2021-06-02 12:05:38 -0500

    Initial Commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by david on 02/06/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Decodable {
+    let rate: Double
+}
